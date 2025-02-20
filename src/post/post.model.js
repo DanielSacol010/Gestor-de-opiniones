40f5defb34a9919049a1f4ba5fbcdd4b9b72a569
DanielSacol010 <danieledusc@gmail.com>
2025-02-20 11:02:13 -0600

    se agrega model de post

---
+++ src/post/post.model.js
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose';
+
+const postSchema = Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'category',
+        required: true
+    }
+}, {
+    versionKey: false 
+})
+
+export default model('post', postSchema)
